fcd478f9086d7675f6a12f0a1d10be3e57c23113
Kilka <kashpurov.danil@mail.ru>
2023-03-23 03:03:15 +0300

    14

---
+++ corp.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>supercoolinterstellarcorporation.github.io</title>
+</head>
+<body>
+&nbsp;
+<div><a href="/"><b>Back to supercoolinterstellarcorporation.github.io</b></a></div>
+</body>
+</html>
+
